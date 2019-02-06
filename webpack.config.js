64de52fcbc04956085b32555da630fc90d0b209c
Andrei Stoicescu <andrei.stoicescu@yopeso.com>
2019-02-06 18:10:46 +0200

    Cleanup & CSS config

--- webpack.config.js
+++ webpack.config.js
@@ -23,5 +23,16 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
   }
 };
